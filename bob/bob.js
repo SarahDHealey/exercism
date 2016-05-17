4b856317eefe5282cf61d7273d66c0997f2cbbbb
Sarah Healey <s.deane.healey@gmail.com>
2016-05-17 11:30:43 -0600

    passing tests

--- bob/bob.js
+++ bob/bob.js
@@ -3,12 +3,29 @@
 // convenience to get you started writing code faster.
 //
 
+//
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
 //
-return "Whatever."
-//
+if (input.trim().length === 0) {
+  return "Fine. Be that way!";
+}
+
+if (/[a-z]/i.test(input) && input == input.toUpperCase()) {
+return 'Whoa, chill out!';
+}
+if (input.slice(-1) == "?") {
+  return 'Sure.';
+}
+  return 'Whatever.';
 };
+
+//
+// return "Whoa, chill out!";
+// } else {
+// return "Whatever.";
+// }
+// //
 
 module.exports = Bob;
